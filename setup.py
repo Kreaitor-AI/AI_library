76b9631dd417291130f270105684e42d755d1745
Mohammad Agwan <89778018+marchmello1@users.noreply.github.com>
2024-07-20 13:55:39 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AI_library",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "langchain",
+        "langchain_openai",
+        "openai",
+    ],
+    author="Mohammad Agwan",
+    author_email="mohammad.agwan@somaiya.edu",
+    description="A toolkit for refining search queries, performing Google searches, fetching and processing web content, and generating images using DALL-E.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/marchmello1/AI_library",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
