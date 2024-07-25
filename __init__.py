a516d0f83ddfa5f9ad309af8cbdcbb4e30afb7cb
Mohammad Agwan <89778018+marchmello1@users.noreply.github.com>
2024-07-25 18:13:03 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ from .chat.gpt3_5 import gpt3_5
 from .chat.gpt4omini import gpt4omini
 from .chat.llama3 import llama3
 from .chat.text_to_text import text_to_text
-from .prompt.blog_prompt import blog_prompt
+from .prompts.blog_prompt import blog_prompt
