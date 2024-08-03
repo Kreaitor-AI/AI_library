57b1f1aa795251a7915584454f6dafdbc96ac853
marchmello1 <89778018+marchmello1@users.noreply.github.com>
2024-08-03 12:35:11 +0530

    Update stability.py

--- imagetools/stability.py
+++ imagetools/stability.py
@@ -1,13 +1,21 @@
 import requests
+import boto3
+from botocore.exceptions import NoCredentialsError
 
 class StabilityImageGenerator:
-    def __init__(self, api_key):
+    def __init__(self, api_key, aws_access_key, aws_secret_key, bucket_name):
         self.api_key = api_key
         self.base_url = "https://api.stability.ai/v2beta/stable-image/generate/sd3"
         self.headers = {
             "authorization": f"Bearer {self.api_key}",
             "accept": "image/*"
         }
+        self.s3_client = boto3.client(
+            's3',
+            aws_access_key_id=aws_access_key,
+            aws_secret_access_key=aws_secret_key
+        )
+        self.bucket_name = bucket_name
     
     def generate_image(self, prompt, aspect_ratio="1:1", model="sd3-medium", seed=0, output_format="jpeg", negative_prompt=None):
         data = {
@@ -24,7 +32,7 @@ class StabilityImageGenerator:
         response = requests.post(
             self.base_url,
             headers=self.headers,
-            files={"none": ''},  # Ensures the request is recognized as multipart/form-data
+            files={"none": ''}, 
             data=data
         )
         
@@ -33,12 +41,16 @@ class StabilityImageGenerator:
         else:
             raise Exception(str(response.json()))
     
-    def save_image(self, image_content, file_path):
-        with open(file_path, 'wb') as file:
-            file.write(image_content)
+    def upload_image_to_s3(self, image_content, s3_file_path):
+        try:
+            self.s3_client.put_object(Bucket=self.bucket_name, Key=s3_file_path, Body=image_content)
+            print(f"Image successfully uploaded to S3 at {s3_file_path}")
+            return f"https://{self.bucket_name}.s3.amazonaws.com/{s3_file_path}"
+        except NoCredentialsError:
+            raise Exception("Credentials not available for AWS S3")
 
-def stability(api_key, prompt, aspect_ratio="1:1", model="sd3-medium", seed=0, output_format="jpeg", negative_prompt=None, file_path="output_image.jpeg"):
-    generator = StabilityImageGenerator(api_key)
+def stability(api_key, aws_access_key, aws_secret_key, bucket_name, prompt, aspect_ratio="1:1", model="sd3-medium", seed=0, output_format="jpeg", negative_prompt=None, s3_file_path="output_image.jpeg"):
+    generator = StabilityImageGenerator(api_key, aws_access_key, aws_secret_key, bucket_name)
     image_content = generator.generate_image(
         prompt=prompt,
         aspect_ratio=aspect_ratio,
@@ -47,5 +59,4 @@ def stability(api_key, prompt, aspect_ratio="1:1", model="sd3-medium", seed=0, o
         output_format=output_format,
         negative_prompt=negative_prompt
     )
-    generator.save_image(image_content, file_path)
-    return file_path
+    return generator.upload_image_to_s3(image_content, s3_file_path)
