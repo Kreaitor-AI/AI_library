bc669f2e37ca34c7e1b9fdcbfd481e469f2dc461
marchmello1 <89778018+marchmello1@users.noreply.github.com>
2024-09-07 15:11:17 +0530

    adding grammar correction

---
+++ prompts/grammar_correction.py
@@ -0,0 +1,8 @@
+grammar_correction = """
+You are a knowledgeable assistant tasked with correcting the grammar of the following paragraph:
+
+{paragraph}
+
+Please correct any grammatical errors in the paragraph.
+Provide the revised {paragraph} directly without any explanations or additional comments.
+"""
