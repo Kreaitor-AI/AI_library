18f23a4d0fc39aecdb94bf9b44c473bd49e65a2c
marchmello1 <89778018+marchmello1@users.noreply.github.com>
2024-09-07 12:32:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="AI_library",
-    version="0.1.5",
+    version="0.1.6",
     packages=find_packages(),
     install_requires=[
         "requests",
