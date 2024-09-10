a3b2772e57cc8f8aba892e4345b6b09a5d71ed0d
marchmello1 <89778018+marchmello1@users.noreply.github.com>
2024-09-10 16:38:52 +0530

    Update __init__.py

--- prompts/__init__.py
+++ prompts/__init__.py
@@ -3,3 +3,4 @@ from .essay_format import essay_format
 from .article_format import article_format
 from .template_blog import template_blog
 from .grammar_correction import grammar_correction
+from .email_prompt import email_prompt
