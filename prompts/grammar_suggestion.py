4cc4361ade61071a6f2ee602cf7b9a9d73ac0ce3
marchmello1 <89778018+marchmello1@users.noreply.github.com>
2024-09-16 15:33:16 +0530

    Update grammar_suggestion.py

--- prompts/grammar_suggestion.py
+++ prompts/grammar_suggestion.py
@@ -1,6 +1,1 @@
-from langchain_core.prompts import PromptTemplate
-grammar_suggestion = PromptTemplate(
-    template="Identify all sentences or words that can be improved and suggest better alternatives for each one (like Grammarly).\n{format_instructions}\n.Here is the paragraph to be corrected{paragraph}\n",
-    input_variables=["paragraph"],
-    partial_variables={"format_instructions": parser.get_format_instructions()},
-)
+grammar_suggestion ="Identify all sentences or words that can be improved and suggest better alternatives for each one (like Grammarly).\n{format_instructions}\n.Here is the paragraph to be corrected{paragraph}\n"
