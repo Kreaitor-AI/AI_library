9b2fb4b5aa1762b1cd4f6690b0cda453bbcd16c6
marchmello1 <89778018+marchmello1@users.noreply.github.com>
2024-09-16 15:20:55 +0530

    Update grammar_suggestion.py

--- prompts/grammar_suggestion.py
+++ prompts/grammar_suggestion.py
@@ -1,5 +1,6 @@
-grammar_suggestion = """
-Identify all sentences or words that can be improved and suggest better alternatives for each one (like Grammarly).
-Provide the original sentence or word, along with an array of alternative suggestions, the sentence's position, and other relevant details.
-Here is the paragraph to be corrected: {paragraph}
-"""
+from langchain_core.prompts import PromptTemplate
+grammar_suggestion = PromptTemplate(
+    template="Identify all sentences or words that can be improved and suggest better alternatives for each one (like Grammarly).\n{format_instructions}\n.Here is the paragraph to be corrected{paragraph}\n",
+    input_variables=["paragraph"],
+    partial_variables={"format_instructions": parser.get_format_instructions()},
+)
