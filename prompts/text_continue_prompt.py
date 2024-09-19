3b556b32fb78a3a7e8ab5c3f83b290fe17835662
marchmello1 <89778018+marchmello1@users.noreply.github.com>
2024-09-19 12:57:12 +0530

    Update text_continue_prompt.py

--- prompts/text_continue_prompt.py
+++ prompts/text_continue_prompt.py
@@ -1,4 +1,4 @@
-blog_continue_prompt = """
+text_continue_prompt = """
 Complete the below text:- {selected_text}. Use below given parameter or input content:-
 tone:- {tone}
 text:- {selected_text}
