6ef9d86e180840b5ac8079f436bfe0cb2b70dcfd
marchmello1 <89778018+marchmello1@users.noreply.github.com>
2024-09-19 13:16:31 +0530

    Update text_maximize_prompt.py

--- prompts/text_maximize_prompt.py
+++ prompts/text_maximize_prompt.py
@@ -1,4 +1,4 @@
-text_maximize_prompt:"""Create a 100 words paragraph of {selected_text}. Use below given parameter or input content:-
+text_maximize_prompt = """Create a 100 words paragraph of {selected_text}. Use below given parameter or input content:-
 context:- {context}
 tone:- {tone}
 text:- {selected_text}"""
