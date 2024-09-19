a4b6499e07a475cc8df46e45e26f21a275485eb7
marchmello1 <89778018+marchmello1@users.noreply.github.com>
2024-09-19 12:49:39 +0530

    Create text_shortner_prompt.py

---
+++ prompts/text_shortner_prompt.py
@@ -0,0 +1,4 @@
+text_shortner_prompt = """ Shorten the below text:- {selected_text} into a 30 words sentence. Use below given parameter or input content:-
+context: - {context}
+tone:- {tone}
+text:- {selected_text}"""
