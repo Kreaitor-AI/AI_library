b68a7a06b05152a2ab29694288252fa03eba3395
marchmello1 <89778018+marchmello1@users.noreply.github.com>
2024-09-19 13:33:25 +0530

    Create text_tone_prompt.py

---
+++ prompts/text_tone_prompt.py
@@ -0,0 +1,4 @@
+text_tone_prompt = """Change the tone of the below text:- {selected_text}. Use below given parameter or input content:-
+context:- {context}
+tone:- {tone}
+text:- {selected_text}"""
