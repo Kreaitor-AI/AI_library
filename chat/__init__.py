202d43346a47873e08bdae1fb52249c2035967c8
marchmello1 <89778018+marchmello1@users.noreply.github.com>
2024-09-20 18:28:48 +0530

    Update __init__.py

--- chat/__init__.py
+++ chat/__init__.py
@@ -3,3 +3,4 @@ from .gpt4o import stream_gpt4omini, non_stream_gpt4omini
 from .llama3 import llama3
 from .text_to_text import text_to_text
 from .gpt4omini import gpt4omini
+from .chatwithdoc import loaddoc, chatwithdoc
