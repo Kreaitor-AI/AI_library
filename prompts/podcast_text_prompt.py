990fc963d5415284b47edc1efee3e9118ba552ac
marchmello1 <89778018+marchmello1@users.noreply.github.com>
2024-10-08 18:12:18 +0530

    Update podcast_text_prompt.py

--- prompts/podcast_text_prompt.py
+++ prompts/podcast_text_prompt.py
@@ -1,4 +1,4 @@
-podcast_text_prompt ="""Generate a podcast script on the topic '{topic}' and the context:{summary_result}. The script should have {num_characters} characters and character names. The script should be approximately {length_words} words long. Please make sure that the script is of {length_words}words. Dont add emotions and extra stuff just give the script
+podcast_text_prompt ="""Generate a podcast script on the topic '{topic}' and the context:{summary_result}. The script should have {num_characters} characters and character names. Make sure to keep {num_characters} only. The script should be approximately {length_words} words long. Please make sure that the script is of {length_words}words. Dont add emotions and extra stuff just give the script
 Dont start with something like *Ben:* .Dont use stars in the script.
 Example:
 Host: Welcome to our podcast, where we discuss all things related to sports and today's topic is India at the Olympics. I'm your host, Aisha, and joining me today are our sports experts – Rahul, Priya, and Arjun.
@@ -6,8 +6,4 @@ Host: Welcome to our podcast, where we discuss all things related to sports and 
 Rahul: India has a long history at the Olympics, dating back to 1900 when Norman Pritchard won two silver medals. Since then, India has been a regular participant at the Summer Games, with the Men's Field Hockey Team being particularly dominant, winning eleven medals between 1928 and 1980.
 
 Priya: The Indian Olympic movement was formalized in the 1920s with the establishment of the Indian Olympic Association. Over the years, India has had success in various sports at the Olympics, including hockey, wrestling, shooting, badminton, and weightlifting.
-
-Arjun: India's best performance at the Olympics came at the 2020 Summer Games, where they won a record seven medals. It was a proud moment for the country and a testament to the hard work and dedication of our athletes.
-
-Aisha: It's inspiring to see India's athletes excel on the world stage and bring home medals for the country. Let's continue to support and cheer for our athletes as they strive for greatness at future Olympic Games.
 """
